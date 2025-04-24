64c161465da8c87f5b594213698263793aa6cb14
alireza sarabadani <srbadni@gmail.com>
2025-04-24 15:25:47 +0330

    push

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -6,7 +6,7 @@ const Comments = () => {
     <div className=''>
       <form className='flex items-center justify-between gap-4 p-4 '>
         <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-          <Image path="general/avatar.png" alt="Lama Dev" w={100} h={100} tr={true}/>
+          <Image path="general/avatar.png" alt="devwithalireza" w={100} h={100} tr={true}/>
         </div>
         <input type="text" className="flex-1 bg-transparent outline-none p-2 text-xl" placeholder="پاسختان را پست کنید"/>
         <button className="py-2 px-4 font-bold bg-white text-black rounded-full">پاسخ</button>
